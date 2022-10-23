4f257d2512af2a736c333fa584ce519ab7ea1b1b
Alexandr <jwchaganav@gmail.com>
2022-10-23 18:07:34 +0400

    Функция готова

--- Program.cs
+++ Program.cs
@@ -1,3 +1,16 @@
 ﻿// Создадим функцию замены символов
 
 Console.WriteLine("Hello, World!");
+
+string Replace(string strInput, char charFind, char charNew)
+{
+    string strNew = String.Empty;
+    for(int i = 0; i < strInput.Length; i++)
+    {
+        if (strInput[i] == charFind)
+            strNew = strNew + $"{charNew}";
+        else
+            strNew = strNew + $"{strInput[i]}";
+    }
+    return strNew;
+}
